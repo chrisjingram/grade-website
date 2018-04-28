9c87b2c3c8a9d42f56d009ba57a4173b4082da2c
Chris Ingram <chris@chrisingram.me>
2018-04-28 16:11:53 +0100

    Added scrolling to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,32 @@ import Link from 'gatsby-link';
 import "../css/nav.css";
 
 export default class NavBar extends React.PureComponent{
+
+  componentDidMount(){
+    console.log("IndexPage componentDidMount");
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        console.log(anchor);
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: "start",
+                inline: "nearest"
+            });
+        });
+    });
+  }
+
   render(){
 
     return (
         <ul className="nav">
 
-          <li><Link to="#">Grade</Link></li>
-          <li><Link to="#">Barber</Link></li>
-          <li><Link to="#">Shop</Link></li>
-          <li><Link to="#">Book</Link></li>
-          <li><Link to="#">Find Us</Link></li>
+          <li><a href="#header">Grade</a></li>
+          <li><a href="#intro">Barber</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#book-now">Book</a></li>
+          <li><a href="#opening-hours">Find Us</a></li>
 
         </ul>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import Footer from "../components/Footer";
 
 export default class IndexPage extends React.PureComponent{
 
+
   render(){
     return (
       <div>
